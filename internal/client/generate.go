3fa3821f5e09dbf06c87c88559cb478273154b12
kodehat <dev@codehat.de>
2025-12-31 10:50:21 +0100

    chore: move config to client package

--- internal/client/generate.go
+++ internal/client/generate.go
@@ -1,3 +1,3 @@
 package client
 
-//go:generate go tool oapi-codegen -config ../../cfg.yaml "https://servercontrolpanel.de/scp-core/api/v1/openapi"
+//go:generate go tool oapi-codegen -config cfg.yaml "https://servercontrolpanel.de/scp-core/api/v1/openapi"
