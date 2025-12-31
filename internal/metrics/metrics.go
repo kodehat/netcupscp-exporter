64bad8922afed54aa67a6e3c29ac286fddd2f41b
kodehat <dev@codehat.de>
2025-12-31 12:11:11 +0100

    chore: change metric namespace

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -6,108 +6,110 @@ import (
 	"github.com/prometheus/client_golang/prometheus/collectors"
 )
 
+const metricsNamespace = "ncscp"
+
 var (
 	buildInfo = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "build_info",
 			Help:      "A metric with a constant '1' value labeled by goversion, revision and version from which netcupscp-exporter was built",
 		},
 		[]string{"goversion", "revision", "version"})
 	cpuCores = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "cpu_cores",
 			Help:      "Number of CPU cores",
 		},
 		[]string{"servername", "servernickname"})
 	memory = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "memory_bytes",
 			Help:      "Amount of memory in bytes",
 		},
 		[]string{"servername", "servernickname"})
 	monthlyTrafficIn = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "monthlytraffic_in_bytes",
 			Help:      "Monthly traffic incoming in bytes (only gigabyte-level resolution)",
 		},
 		[]string{"servername", "servernickname", "month", "year", "mac"})
 	monthlyTrafficOut = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "monthlytraffic_out_bytes",
 			Help:      "Monthly traffic outgoing in bytes",
 		},
 		[]string{"servername", "servernickname", "month", "year", "mac"})
 	monthlyTrafficTotal = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "monthlytraffic_total_bytes",
 			Help:      "Total monthly traffic in bytes",
 		},
 		[]string{"servername", "servernickname", "month", "year", "mac"})
 	serverStartTimeSeconds = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "server_start_time_seconds",
 			Help:      "Start time of the servername in seconds",
 		},
 		[]string{"servername", "servernickname"})
 	serverIpInfo = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "ip_info",
 			Help:      "Ip addresses assigned to this server",
 		},
 		[]string{"servername", "servernickname", "mac", "ip", "type"})
 	ifaceThrottled = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "interface_throttled",
 			Help:      "Interface's traffic is throttled (1) or not (0)",
 		},
 		[]string{"servername", "servernickname", "mac", "status"})
 	serverStatus = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "server_status",
 			Help:      "Online (1) / Offline (0) status",
 		},
 		[]string{"servername", "servernickname", "status"})
 	rescueActive = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "rescue_active",
 			Help:      "Rescue system active (1) / inactive (0)",
 		},
 		[]string{"servername", "servernickname", "status"})
 	rebootRecommended = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "reboot_recommended",
 			Help:      "Reboot recommended (1) / not recommended (0)",
 		},
 		[]string{"servername", "servernickname", "status"})
 	diskCapacity = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "disk_capacity_bytes",
 			Help:      "Available storage space in bytes",
 		},
 		[]string{"servername", "servernickname", "driver", "name"})
 	diskUsed = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "disk_used_bytes",
 			Help:      "Used storage space in bytes",
 		},
 		[]string{"servername", "servernickname", "driver", "name"})
 	diskOptimization = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "scp",
+			Namespace: metricsNamespace,
 			Name:      "disk_optimization",
 			Help:      "Optimization recommended (1) / not recommended (0)",
 		},
